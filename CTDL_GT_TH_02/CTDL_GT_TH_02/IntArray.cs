25127dbf06d4640b680ae2e1ab3f672b2e0b6540
quangltm1 <112951981+quangltm1@users.noreply.github.com>
2022-09-26 20:23:12 +0700

    Add files via upload

---
+++ CTDL_GT_TH_02/CTDL_GT_TH_02/IntArray.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTDL_GT_TH_02
+{
+    class IntArray
+    {
+        private int[] arr { get; set; }
+        private int k { get; set; }
+        private int x { get; set; }
+
+
+        public IntArray() { }
+        public IntArray(int k)
+        {
+            this.arr = new int[k];
+            
+        }
+
+        public IntArray(int[] arr)
+        {
+            
+        }
+
+        public IntArray(IntArray obj) { }
+
+
+        public void Nhap()
+        {
+            for (int i = 0; i< arr.Length; i++)
+            {
+                Console.Write("arr[" + i + "]=");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public bool KiemTraKT(int k)
+        {
+            while (k < 1 || k > 200)
+            {
+
+                Console.WriteLine("Nhap lai kich thuoc mang: ");
+                k = Int32.Parse(Console.ReadLine());
+
+            }
+            return true;
+        }
+
+        public void Xuat()
+        {
+            Console.Write("Hien thi mang:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        public int TimTuanTu(int x)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        public int TimNhiPhan(int x)
+        {
+            int left , right ,mid; left = 0; right = arr.Length - 1;
+
+            do
+            {
+                mid = (left + right)/2;
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }else if(arr[mid] < x)
+                {
+                    left = mid + 1;
+                }else if (arr[mid] > x)
+                {
+                    right = mid - 1;
+                }
+            } while (left <= right);
+            return -1;
+        }
+    }
+
+    
+}
---
+++ CTDL_GT_TH_02/CTDL_GT_TH_02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CTDL_GT_TH_02;
+
+namespace CTDLGT_TH_02
+{
+    class Program
+    {
+        static void TestTimTuanTu()
+        {
+            int k, x, kq;
+            Console.WriteLine("TIM KIEM TUAN TU");
+            Console.Write(">>Nhap so luong mang: ");
+            int.TryParse(Console.ReadLine(), out k);
+            IntArray obj = new IntArray(k);
+            obj.KiemTraKT(k);
+            obj.Nhap();
+            Console.WriteLine(">>Cac phan tu:");
+            obj.Xuat();
+            Console.Write("\n>>Gia tri can tim x = ");
+            int.TryParse(Console.ReadLine(), out x);
+            kq = obj.TimTuanTu(x);
+            if (kq == -1)
+            {
+                Console.WriteLine("->Khong ton tai {0}!", x);
+            }
+            else
+                Console.WriteLine("->Co {0} tai vi tri {1} ", x, kq);
+
+
+        }
+        static void TestTimNhiPhan()
+        {
+            int k, x, kq;
+            Console.WriteLine("TIM KIEM NHI PHAN");
+            Console.Write(">>Nhap so luong mang: ");
+            int.TryParse(Console.ReadLine(), out k);
+            IntArray obj = new IntArray(k);
+            obj.KiemTraKT(k);
+            obj.Nhap();
+            Console.WriteLine(">>Cac phan tu:");
+            obj.Xuat();
+            Console.Write("\n>>Gia tri can tim x = ");
+            int.TryParse(Console.ReadLine(), out x);
+            kq = obj.TimNhiPhan(x);
+            if (kq == -1)
+            {
+                Console.WriteLine("->Khong ton tai {0}!", x);
+            }
+            else
+                Console.WriteLine("->Co {0} tai vi tri {1} ", x, kq);
+        }
+        static void Main(string[] args)
+        {
+            TestTimTuanTu();
+            TestTimNhiPhan();
+        }
+    }
+}
